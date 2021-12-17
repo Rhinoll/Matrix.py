a04ce063e276401680e058c67dcdac66520de35c
Andrew <zenit.yfon@gmail.com>
2021-12-17 21:10:02 +0300

    v3

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import random
 
+def ContinueCreator():
+    contin = input("\nЕсли хотите начать заново, нажмите enter, для остановки - 'x' \n")
+
+    if contin == 'x':
+        exit()
+    else:
+        return True
+
 def Sort(matrix):
 
-    sortedMartixUpper = []
-    sortedMartixDown = []
-
-    print("\nОтсортированный массив: \n")
+    sortedMatrixUpper = []
+    sortedMatrixDown = []
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if (matrix[i][j]) % 2 == 0:
-                sortedMartixUpper.append(matrix[i][j])
+                sortedMatrixUpper.append(matrix[i][j])
             else:
-                sortedMartixDown.append(matrix[i][j])
+                sortedMatrixDown.append(matrix[i][j])
 
-    sortedMartixUpper.sort() #Сортируем по возрастанию
-    sortedMartixDown.sort(reverse=True) #Сортируем по убыванию
+    sortedMatrixUpper.sort() #Сортируем по возрастанию
+    sortedMatrixDown.sort(reverse=True) #Сортируем по убыванию
 
-    finalSort = sortedMartixUpper + sortedMartixDown #Собираем масссивы
+    finalSort = sortedMatrixUpper + sortedMatrixDown #Собираем масссивы
+
+    print("\nОтсортированный массив: \n")
     print(finalSort)
+
+    if ContinueCreator(): #Если хотим продолжить - продолжаем
+        Do()
 
 def MatrixRandomCreator(m): #Функция создания случайной матрицы
 
@@ -37,32 +48,43 @@ def MatrixSelfCreator(m): #Функция создания матрицы вру
 
     matrix = []
 
-    print("Вы выбрали размер матрицы ", m, "x", m, " не выходите за них - это остановит программу!\nВведите значения с клавиатуры: \n")
+    print("Вы выбрали размер матрицы ", m, "x", m, ", не выходите за них - это остановит программу!\nВведите значения с клавиатуры: \n")
 
     for i in range(m):
         row = input().split()
 
         if len(row) > m:
             print("Вы превысили границы, которые сами ввели!")
-            exit()
+
+            if ContinueCreator():
+                MatrixSelfCreator(m)
 
         elif len(row) < m:
             print("Вы недостали границы, которую сами ввели!")
-            exit()
+
+            if ContinueCreator():
+                MatrixSelfCreator(m)
 
         for i in range(len(row)):
             try:
                 row[i] = int(row[i])
             except ValueError:
                 print("Нужно вводить только целые числа, а не строки или дробные числа!")
-                exit()
+
+                if ContinueCreator():
+                    MatrixSelfCreator(m)
 
             if row[i] > 100:
                 print("По условию задачи, числа не могут превышать сотни!")
-                exit()
+
+                if ContinueCreator():
+                    MatrixSelfCreator(m)
+
             elif row[i] < 1:
                 print("По условию задачи, числа не могут быть меньше еденицы!")
-                exit()
+
+                if ContinueCreator():
+                    MatrixSelfCreator(m)
 
         matrix.append(row)
 
@@ -75,26 +97,39 @@ def MatrixSelfCreator(m): #Функция создания матрицы вру
 
     Sort(matrix) #Вызываем логику программы
 
+def Do():
+
+    while True:
+
+        try:
+            m = int(input("Введите число от 2 до 5 - размер матрицы: "))
+        except ValueError:
+            print("Нужно вводить только целые числа, а не строки или дробные числа! ")
+
+            if ContinueCreator():
+                continue
+
+        if m < 2 or m > 5:
+            print("Вы ввели неверное значение, размер матрицы должен соответсвовать диапазону из задания!")
+
+            if ContinueCreator():
+                continue
+
+        ask = input("Матрица содержит только целые цисла в диапазоне от 1 до 100.\nХотите заполнить матрицу случайно - "
+                    "введите 'y', если с клавиатуры введите 'n': ")
+
+        if ask == 'y':
+            MatrixRandomCreator(m)
+
+        elif ask == 'n':
+            MatrixSelfCreator(m)
+
+        else:
+            print("Нужно вводить 'y' или 'n', а не числа или другие буквы!")
+
+            if ContinueCreator():
+                continue
+
 if __name__ == '__main__':
 
-    try:
-        m = int(input("Введите число от 2 до 5 - размер матрицы: "))
-    except ValueError:
-        print("Нужно вводить только целые числа, а не строки или дробные числа!")
-        exit()
-
-    if m < 2 or m > 5:
-        print("Вы ввели неверное значение, размер матрицы должен соответсвовать диапазону из задания!")
-        exit()
-
-    ask = input("Матрица содержит только целые цисла в диапазоне от 1 до 100.\nХотите заполнить матрицу случайно - "
-                "введите 'y', если с клавиатуры введите 'n': ")
-
-    if ask == 'y':
-        MatrixRandomCreator(m)
-
-    elif ask == 'n':
-        MatrixSelfCreator(m)
-
-    else:
-        print("Нужно вводить 'y' или 'n', а не числа или другие буквы!")
+    Do() #Запускаем функцию
